4015de5600d7e1bf063b67e325417d87925b9e78
ArnavYadnopavit <yadnopavitarnav@gmail.com>
2024-09-29 13:39:09 +0530

    revise

--- Assignments/matgeo/Assignment4/codes/code.c
+++ Assignments/matgeo/Assignment4/codes/code.c
@@ -45,12 +45,48 @@ int main()
     }
 
     point_gen(fptr, A, B, 10);
-    point_gen(fptr, B, C, 20);
-    point_gen(fptr, C, A, 20);
+    point_gen(fptr, B, C, 10);
+    point_gen(fptr, C, A, 10);
 
     freeMat(A, m);
     freeMat(B, m) ;
     freeMat(C, m) ;
 
     fclose(fptr) ;
+    //original triangle
+    double ao, bo, co, x1o, y1o, x2o, y2o, x3o, y3o, anglebo;
+    ao = 6;
+    co =  5;
+    anglebo = 60;
+    bo = sqrt(pow(ao,2)+pow(co,2)- (2*ao*co*cos(anglebo)));
+    
+    int mo = 2, no = 1;
+    double **Ao = createMat(mo, no);
+    double **Bo = createMat(mo, no);
+    double **Co = createMat(mo, no);
+    double radianbo = anglebo*M_PI/180;
+
+    Ao[0][0] = co*cos(radianbo);
+    Ao[1][0] = co*sin(radianbo);
+    Bo[0][0] = 0;
+    Bo[1][0] = 0;
+    Co[0][0] = ao;
+    Co[1][0] = 0;
+
+    FILE *fptro;
+    fptro = fopen("triangleo_points.txt", "w");
+    if (fptro == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    point_gen(fptro, Ao, Bo, 10);
+    point_gen(fptro, Bo, Co, 10);
+    point_gen(fptro, Co, Ao, 10);
+
+    freeMat(Ao, mo);
+    freeMat(Bo, mo) ;
+    freeMat(Co, mo) ;
+
+    fclose(fptro) ;
 }
--- Assignments/matgeo/Assignment4/codes/plot.py
+++ Assignments/matgeo/Assignment4/codes/plot.py
@@ -2,26 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# Load the points from the text file
 points = np.loadtxt("triangle_points.txt", delimiter=',')
 
-# Extract the x and y coordinates
 x = points[:, 0]
 y = points[:, 1]
 
-# Since your C code generates points along one edge of the triangle, 
-# we need to manually add the other two vertices to form the triangle.
 x_full = np.append(x, [x[0]])
 y_full = np.append(y, [y[0]])
-
+ABx=x_full[0:12]
+ABy=y_full[0:12]
+BCx=x_full[11:24]
+BCy=y_full[11:24]
+ACx=x_full[22:34]
+ACy=y_full[22:34]
 # Plot the triangle
 plt.figure()
-plt.plot(x_full, y_full, label='Triangle Edges')
-plt.fill(x_full, y_full, 'lightblue', alpha=0.5)
+plt.plot(ABx, ABy, label='AB\'')
+plt.plot(BCx, BCy, label='BC\'')
+plt.plot(ACx, ACy, label='CA\'')
+
+points = np.loadtxt("triangleo_points.txt", delimiter=',')
+
+xo = points[:, 0]
+yo = points[:, 1]
+
+xo_full = np.append(xo, [xo[0]])
+yo_full = np.append(yo, [yo[0]])
+ABxo=xo_full[0:12]
+AByo=yo_full[0:12]
+BCxo=xo_full[11:24]
+BCyo=yo_full[11:24]
+ACxo=xo_full[22:34]
+ACyo=yo_full[22:34]
+# Plot the triangle
+plt.plot(ABxo, AByo,linestyle='dotted', label='AB')
+plt.plot(BCxo, BCyo,linestyle='dotted', label='BC')
+plt.plot(ACxo, ACyo,linestyle='dotted', label='CA')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True)
 plt.legend()
+
+print(xo_full)
+plt.annotate("B", (0,0) ,textcoords="offset points", xytext=(0, 10), ha='center')
+plt.annotate("A", (2.500000,4.330127) ,textcoords="offset points", xytext=(0, 10), ha='center')
+plt.annotate("C", (6.000000,0.000000),textcoords="offset points", xytext=(0, 10), ha='center')
+plt.annotate("A\'", (1.785714,3.092948) ,textcoords="offset points", xytext=(0, 10), ha='center')
+plt.annotate("C\'", (4.285714,0.000000) ,textcoords="offset points", xytext=(0, 10), ha='center')
 
 # Save the plot to figs directory
 plt.savefig('../figs/fig.png')
