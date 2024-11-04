bc97159bd5ea6a127351de0eec50cd7f6d7582be
ArnavYadnopavit <yadnopavitarnav@gmail.com>
2024-11-04 21:47:32 +0530

    revise

--- Assignments/matgeo/Assignment5/codes/datagen.c
+++ Assignments/matgeo/Assignment5/codes/datagen.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 
 #include "libs/matfun.h"
 #include "libs/geofun.h"
